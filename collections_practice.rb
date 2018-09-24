fe3c8223be34e1971204ddd92f667c48377870ff
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-24 18:57:42 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,3 +21,9 @@ def remove_non_strings(array)
     word.is_a?(String) == false 
   end 
 end 
+
+def count_elements(array)
+  array.uniq.each {|i| count = 0 
+  array.each {|i2| if i2 == i then count += 1 end} 
+  i[:count] = count} 
+end 
