4a135dedc391fa1f91af348ec14063abb6c3134e
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-24 19:05:40 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,13 +39,10 @@ end
 def find_cool(cool)
   cool.select {|i| i.any? {|k,v| v == "cool}}"
 end 
-end
+
 
 def organize_schools(schools)
   school_locations= {} 
   schools.collect {|k,v| school_locations[v[:location]] = []} 
   school_locations.each {|k,v| schools.each {|k1,v1| if k == v1[:location] then v << k1 end}}
-end 
-end 
-end 
 end 
